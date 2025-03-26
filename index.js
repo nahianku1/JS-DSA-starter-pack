4c67826d9b9366e00cd68af3bb2e43a85203a20c
himel <himelku1@gmail.com>
2025-03-26 20:31:07 +0600

    problem_link added commit

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ import cout from "./output.js";
 
 let n = Number(await cin());
 let sum = 0;
+let t1 = performance.now();
 for (let i = 0; i <= n; ++i) {
   sum += i;
 }
 // sum = (n * (n + 1)) / 2;
+let t2 = performance.now();
 
 cout(sum);
+cout(t2 - t1);
